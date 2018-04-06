ac200ce92e48a0fa4ba116e134a9ef6fd1ae8cb7
Mikael <m.redon@outlook.fr>
2018-04-07 00:01:07 +0200

    add Parser

---
+++ src/main/java/rpn/Parser/Parser.java
@@ -0,0 +1,24 @@
+package rpn.Parser;
+
+import java.util.Stack;
+
+public class Parser {
+    public final String symbol;
+    Parser(String symbol){
+        this.symbol = symbol;
+    }
+
+    Parser(){
+        this.symbol = " ";
+    }
+
+    public Stack<String> parse(String string){
+        Stack<String> result = new Stack<>();
+
+        for (String token : string.split(this.symbol)) {
+            result.push(token);
+        }
+
+        return result;
+    }
+}
---
+++ src/test/java/rpn/Parser/PerserTest.java
@@ -0,0 +1,57 @@
+package rpn.Parser;
+
+import org.junit.Test;
+
+import java.util.Stack;
+
+import static org.junit.Assert.assertEquals;
+
+public class PerserTest {
+
+    @Test
+    public void should_split_text_on_space(){
+        Parser parser = new Parser();
+
+        String str = "should split text on space";
+
+        Stack<String> toCompare = new Stack<>();
+
+        toCompare.push("should");
+        toCompare.push("split");
+        toCompare.push("text");
+        toCompare.push("on");
+        toCompare.push("space");
+
+        assertEquals(toCompare, parser.parse(str));
+    }
+
+    @Test
+    public void should_split_text_on_ampersand(){
+        Parser parser = new Parser("&");
+
+        String str = "should&split&text&on&space";
+
+        Stack<String> toCompare = new Stack<>();
+
+        toCompare.push("should");
+        toCompare.push("split");
+        toCompare.push("text");
+        toCompare.push("on");
+        toCompare.push("space");
+
+        assertEquals(toCompare, parser.parse(str));
+    }
+
+    @Test
+    public void should_not_split_text(){
+        Parser parser = new Parser();
+
+        String str = "should&split&text&on&space";
+
+        Stack<String> toCompare = new Stack<>();
+
+        toCompare.push("should&split&text&on&space");
+
+        assertEquals(toCompare, parser.parse(str));
+    }
+}
